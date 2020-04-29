8d423fdd2010e0cc6ca8ceff654876e25705f953
Sebastian <skrivtilsebastian99@gmail.com>
2020-04-29 12:56:46 +0200

    Made index fetch posts.

--- js/uploud_posts.js
+++ js/uploud_posts.js
@@ -1,7 +1,16 @@
-axios.get('/php/hent_bolgpost.php', {params: {posts: 'getallpost'}})
+axios.get('/php/hent_posts.php', {params: {posts: 'getallpost'}})
     .then(function (response) {
-        console.log(response);
+        console.log(response.data);
+		let data = response.data;
+		// document.getElementById("print").innerHTML = data[0].tekst;
+
+		for(let i = 0; i < data.length; i++) {
+		    console.log(data[i]);
+            createPost(document.getElementById("posts"), data[i]);
+        }
+
     })
     .catch(function (error) {
         console.log(error);
     });
+
