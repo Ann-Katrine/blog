79df77e6f743eb523c81ab287ce2649e6bf35991
Sebastian <skrivtilsebastian99@gmail.com>
2020-05-07 12:45:20 +0200

    Did not Fix grafRead.php

--- php/class_statik.php
+++ php/class_statik.php
@@ -1,28 +1,28 @@
 <?php
 	include_once('db.php');
-	include_once('statik.php');
+	include_once('tabler/statik.php');
 
 	class statiker{
 		public function getStatikPrDag($id){
 			$DB = new DB();
-			
+
 			$stmt = $DB->conn->prepare("SELECT number_Read FROM statik WHERE Postid = ?");
-			
+
 			$stmt->bind_param("i", $id);
 			$stmt->execute();
-			
+
 			$stmt->close();
 			$DB->conn->close();
 		}
-		
+
 		public function countHowManyReadOnOnePost($id){
 			$DB = new DB();
-			
+
 			$stmt = $DB->conn->prepare("SELECT COUNT(number_read) FROM statik WHERE postid = ?");
-			
+
 			$stmt->bind_param("i", $id);
 			$stmt->execute();
-			
+
 			$stmt->close();
 			$DB->conn->close();
 		}
