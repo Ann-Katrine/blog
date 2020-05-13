42aa5ba63b5cb20203316680c3c8ab5733cff992
Ann-Katrine Nykjær <annx3683@skpit.local>
2020-05-13 10:34:00 +0200

    graf read virker

--- php/class_follows.php
+++ php/class_follows.php
@@ -67,7 +67,7 @@
 			$DB->conn->close();
 		}
 
-		public function getHowManyFollowsOnADay($date1){
+		public function countHowManyFollowsOnADay($date1){
 			$DB = new DB();
 
 			$stmt = $DB->conn->prepare("SELECT COUNT(idFollows) FROM Follows WHERE dato =  ?");
@@ -81,7 +81,7 @@
 			$DB->conn->close();
 		}
 
-		public function getHowManyFollowsOnAMonth($date1, $date2){
+		public function countHowManyFollowsOnAMonth($date1, $date2){
 			$DB = new DB();
 
 			$stmt = $DB->conn->prepare("SELECT COUNT(idFollows) FROM Follows WHERE dato BETWEEN ? AND ?");
@@ -93,6 +93,38 @@
 			
 			$stmt->close();
 			$DB->conn->close();
+		}
+		
+		public function getDatoFromToDato($date1, $date2){
+			$DB = new DB();
+
+			$stmt = $DB->conn->prepare("SELECT dato FROM Follows WHERE dato BETWEEN ? AND ?");
+
+			$stmt->bind_param("s", $dato1, $dato2));
+			$stmt->execute();
+			
+			$data = $stmt->get_result()->fetch_all(MYSQLI_ASSOC)/*["number_Read"]*/;
+			
+			$stmt->close();  
+			$DB->conn->close();
+			
+			return $data;
+		}
+		
+		public function countHowManyDatoFromTwoDato($date1, $date2){
+			$DB = new DB();
+
+			$stmt = $DB->conn->prepare("SELECT COUNT(dato) AS dato FROM Follows WHERE dato BETWEEN ? AND ?");
+
+			$stmt->bind_param("s", $dato1, $dato2));
+			$stmt->execute();
+			
+			$data = $stmt->get_result()->fetch_all(MYSQLI_ASSOC)/*["number_Read"]*/;
+			
+			$stmt->close();  
+			$DB->conn->close();
+			
+			return $data;
 		}
 	}
 ?>
