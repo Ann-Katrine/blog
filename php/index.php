5f4ca889b46fe0589c9bcf7e3f71aee2770cf987
Ann-Katrine Nykjær <annx3683@skpit.local>
2020-05-13 09:59:02 +0200

    prøver at få database tekst

--- php/index.php
+++ php/index.php
@@ -87,7 +87,7 @@
 	}); */
 
 /************************************************/
-/*                  follow graf                 */
+/*                  follow graf                 */ // ikke færdig
 /************************************************/
 	Route::add('/post/follows/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))', function($date1){
 		include_once("./class_follows.php");
@@ -132,17 +132,18 @@
 
 		// test til tekst
 		$font = "/home/sebathefox/domains/ak.sebathefox.dk/public_html/php/graf/arial.ttf";
-		$in = 5;
-		$tal = 20;
+		//$in = 5;
+		$tal = 28;
 		$linje = 310;
 		for($i = 0; $i <= $tal; $i++){
 			imageline($img, 20, $linje, 320, $linje, $black);
 			$linje = $linje - 10;
-			if($i == 5){
-				imagettftext($img, 10, 360, 7, 274, $black, $font, $in); // for at få tekst. 20 er størrelse, 350 er balacen for tallet, 210 y-aske, 200 x-akse
-			}
 		}
-		
+		imagettftext($img, 10, 360, 7, 274, $black, $font, "5"); // for at få tekst. 10 er størrelse, 360 er balacen for tallet, 274 y-aske, 7 x-akse
+		imagettftext($img, 10, 360, 3, 224, $black, $font, "10");
+		imagettftext($img, 10, 360, 3, 174, $black, $font, "15");
+		imagettftext($img, 10, 360, 3, 124, $black, $font, "20");
+		imagettftext($img, 10, 360, 3, 74, $black, $font, "25");
 
 		// sætter header til PNG
 		header('Content-Type: image/png');
@@ -155,7 +156,7 @@
 	});
 
 /************************************************/
-/*             number read graf                 */
+/*             number read graf                 */ // ikke færdig
 /************************************************/
 	Route::add('/post/read/([0-9]*)', function($id){
 //		include_once("./graf/grafRead.php");
@@ -185,18 +186,22 @@
 		$statiker = new statiker();
 
 		$antal = $statiker->countHowManyReadOnOnePost($id);
-		/*$antalRead = $statiker->getStatikPrDag($id);
-		for($i = 1; $i <= $antal; $i++){
-			imagefilledrectangle($img, 40, 320, 80, 320-($antalRead*10), $red);
-			imagerectangle($img, 40, 320, 80, 320-($antalRead*10), $black);
-		}*/
-
+		
+		$font = "/home/sebathefox/domains/ak.sebathefox.dk/public_html/php/graf/arial.ttf";
+		$x_akseTal = 35;
 		$antalRead = array_values($statiker->getStatikPrDag($id));
 		for($i = 0; $i <= count($antalRead); $i++){
 
 			imagefilledrectangle($img, $i * 40 + 25, 320, $i * 40 + 60, 320-($antalRead[$i]["number_Read"] * 10), $red);
 			imagerectangle($img, $i * 40 + 25, 320, $i * 40 + 60, 320-($antalRead[$i]["number_Read"] * 10), $black);
         }
+		
+		$antalDato = array_values($statiker->countHowManyDatoOnOnePost($id));
+		$datoRead = array_values($statiker->getDatoPrDag($id));
+		for($i = 0; $i <= $antalDato; $i++){
+			imagettftext($img, 10, 330, $x_akseTal, 334, $black, $font, $datoRead[$i]);
+			$x_akseTal = $x_akseTal + 40;
+		}
 
 		/*// Cats: 6
         imagefilledrectangle($img, 40, 320, 90, 320-(6*35), $red);
@@ -221,22 +226,22 @@
 		// laver y-axis
 		imageline($img, 20, 320, 20, 320-(8*35)-20, $black);
 
-		// test til tekst
-		//$text = "1";
-		$font = "/home/sebathefox/domains/ak.sebathefox.dk/public_html/php/graf/arial.ttf";
-		$in = 5;
-		imageline($img, 20, 310, 320, 310, $black);	//
-		imageline($img, 20, 300, 320, 300, $black);
-		imageline($img, 20, 290, 320, 290, $black);
-		imageline($img, 20, 280, 320, 280, $black);
-		imageline($img, 20, 270, 320, 270, $black);
-		imageline($img, 20, 260, 320, 260, $black);
-		imageline($img, 20, 250, 320, 250, $black);
-		imageline($img, 20, 240, 320, 240, $black);
-		imageline($img, 20, 230, 320, 230, $black);
-		imageline($img, 20, 220, 320, 220, $black);
-		imageline($img, 20, 210, 320, 210, $black);
-		imagettftext($img, 10, 360, 7, 274, $black, $font, $in); // for at få tekst. 20 er størrelse, 350 er balacen for tallet, 210 y-aske, 200 x-akse
+		// tekst på y-aksesn
+		$tal = 28; 
+		$linje = 310;
+		for($i = 0; $i <= $tal; $i++){
+			imageline($img, 20, $linje, 320, $linje, $black);
+			$linje = $linje - 10;
+		}
+		imagettftext($img, 10, 360, 7, 274, $black, $font, "5"); // for at få tekst. 10 er størrelse, 360 er balacen for tallet, 274 y-aske, 7 x-akse
+		imagettftext($img, 10, 360, 3, 224, $black, $font, "10");
+		imagettftext($img, 10, 360, 3, 174, $black, $font, "15");
+		imagettftext($img, 10, 360, 3, 124, $black, $font, "20");
+		imagettftext($img, 10, 360, 3, 74, $black, $font, "25");
+		
+		// tekst på x-aksen
+		imagettftext($img, 10, 330, 35, 334, $black, $font, "god dag var");
+		imagettftext($img, 10, 330, 75, 334, $black, $font, "god dag var");
 
 		// sætter header til PNG
 		header('Content-Type: image/png');
